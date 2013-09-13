abced780466927171336c911a8c6639dacc161b7
Calvin Fernandez <cfernand@cs.stanford.edu>
2013-09-13 13:21:45 -0700

    added options sidebar

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,10 +4,33 @@ angular.module('snippetUiApp', ['ui.router'])
   .run(function($rootScope, $state, $stateParams) {
     $rootScope.$state = $state;
     $rootScope.$stateParams = $stateParams;
-  });
-/*
-  .config(function ($routeProvider) {
-    $routeProvider
+  })
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('inbox', {
+        url: '/inbox',
+        templateUrl: 'views/inbox.html',
+        controller: 'MainCtrl'
+      })
+      .state('inbox.options', {
+        url: '',
+        templateUrl: 'views/inbox.options.html',
+        onEnter: function(){
+          var inbox = document.getElementById('inbox-container');
+          var width = inbox.offsetWidth;
+          inbox.style.width = width + 'px';
+          inbox.parentNode.style.overflow = 'hidden';
+          inbox.style.marginLeft = '75%';
+        },
+        onExit: function(){
+          var inbox = document.getElementById('inbox-container');
+          inbox.parentNode.style.overflow = 'auto';
+          inbox.style.width = 'inherit';
+          inbox.style.position = 'static';
+          inbox.style.marginLeft = 'auto';
+        }
+      })
+    /*$routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
@@ -22,5 +45,5 @@ angular.module('snippetUiApp', ['ui.router'])
       })
       .otherwise({
         redirectTo: '/'
-      });
-  }); */
+      });*/
+  }); 
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,9 +1,20 @@
 'use strict';
 
 angular.module('snippetUiApp')
-  .controller('MainCtrl', function ($scope, Conversations) {
+  .controller('MainCtrl', function ($scope, $state, Conversations) {
 
   $scope.conversations = Conversations.all();
   $scope.me = Conversations.me();
 
+  $scope.header = {
+    title: 'Inbox'
+  }
+
+  $scope.toggleOptions = function() {
+    if ($state.current.name === 'inbox.options') {
+      $state.go('inbox');
+    } else {
+      $state.go('inbox.options');
+    }
+  }
 });
